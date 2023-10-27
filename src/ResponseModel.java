e158886e379a7906db9563fde22b5cc3fb92514c
Myheartdies <97077229+Myheartdies@users.noreply.github.com>
2023-10-27 11:35:43 -0500

    Neil (#2)
    * fix type error of request response model
    
    * Fix spelling error in responsemodel
    
    * Add utility function in remotedao
    implement conn() WIP
    
    ---------
    
    Co-authored-by: Myheartdies <76390200+Reacon01@users.noreply.github.com>

--- src/ResponseModel.java
+++ src/ResponseModel.java
@@ -1,9 +1,9 @@
 public class ResponseModel {
     static public int OK = 0;
-    static public int UNKOWN_REQUEST = 1;
+    static public int UNKNOWN_REQUEST = 1;
     static public int DATA_NOT_FOUND = 2;
     static public int SAVE_FAILED = 3; // Body will contain failure return value
 
     public int code;
-    public int body;
+    public String body;
 }
