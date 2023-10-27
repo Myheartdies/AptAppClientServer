a472a909b3df1882592c4758be52946fbd23d01d
Myheartdies <76390200+Reacon01@users.noreply.github.com>
2023-10-27 11:33:50 -0500

    Fix spelling error in responsemodel

--- src/ResponseModel.java
+++ src/ResponseModel.java
@@ -1,6 +1,6 @@
 public class ResponseModel {
     static public int OK = 0;
-    static public int UNKOWN_REQUEST = 1;
+    static public int UNKNOWN_REQUEST = 1;
     static public int DATA_NOT_FOUND = 2;
     static public int SAVE_FAILED = 3; // Body will contain failure return value
 
